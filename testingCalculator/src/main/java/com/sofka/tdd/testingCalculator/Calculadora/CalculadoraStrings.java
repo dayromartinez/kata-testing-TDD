2f08892725b1a7142d5c9b16803170f032d29e8f
Dayro Martínez <djmd96@hotmail.com>
2022-01-17 19:05:26 -0500

    TESTS OK!

--- testingCalculator/src/main/java/com/sofka/tdd/testingCalculator/Calculadora/CalculadoraStrings.java
+++ testingCalculator/src/main/java/com/sofka/tdd/testingCalculator/Calculadora/CalculadoraStrings.java
@@ -15,25 +15,58 @@ public class CalculadoraStrings {
     public int stringCalculator(String cadena){
 
         int resultado = 0;
+        int tresNumeros = sumaTresNumeros(cadena);
 
-        if(cadena.equals("")){
+        if(cadena.equals("")) {
             resultado = resultado;
         }else if(cadena.length() == 1){
             resultado = Integer.parseInt(cadena);
-        }else if(cadena.length() == 3){
-            resultado = sumasencilla(cadena);
+        }else if(tresNumeros != 0){
+            resultado = tresNumeros;
+        }else if(Integer.parseInt(cadena) < 0) {
+            throw new IllegalArgumentException("Numero negativo");
+        }else if(cadena.contains(",")){
+            resultado = sumasencillaComa(cadena);
+        }else if(cadena.contains("\n")){
+            resultado = sumasencillaSaltoLinea(cadena);
         }
         return resultado;
     }
 
-    private int sumasencilla(String cadena){
+    private int sumasencillaComa(String cadena){
 
         int resultado = 0;
-        System.out.println(cadena.indexOf(","));
         String[] numeros = cadena.split(",");
         int numero1 = Integer.parseInt(numeros[0]);
         int numero2 = Integer.parseInt(numeros[1]);
         resultado = numero1 + numero2;
+        return resultado;
+    }
+
+    private int sumasencillaSaltoLinea(String cadena){
+        int resultado = 0;
+        String[] numeros = cadena.split("\n");
+        int numero1 = Integer.parseInt(numeros[0]);
+        int numero2 = Integer.parseInt(numeros[1]);
+        resultado = numero1 + numero2;
+        return resultado;
+    }
+
+    private int sumaTresNumeros(String cadena){
+
+        int resultado = 0;
+
+        String[] caracteres = {",", " ", "#", "/", "\n"};
+
+        for (int i = 0; i < caracteres.length; i++) {
+            if(cadena.contains(caracteres[i])){
+                var numeros = cadena.split(caracteres[i]);
+                if(numeros.length == 3){
+                  resultado = Integer.parseInt(numeros[0]) + Integer.parseInt(numeros[1]) + Integer.parseInt(numeros[2]);
+                  break;
+                }
+            }
+        }
         return resultado;
     }
 
--- testingCalculator/src/test/java/com/sofka/tdd/testingCalculator/TestingCalculatorApplicationTests.java
+++ testingCalculator/src/test/java/com/sofka/tdd/testingCalculator/TestingCalculatorApplicationTests.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class TestingCalculatorApplicationTests {
 
@@ -34,9 +37,59 @@ class TestingCalculatorApplicationTests {
 	@DisplayName("Dos numeros, delimitados por coma, devuelven la suma.")
 	void sumaPorComa(){
 
-		var cadena = new CalculadoraStrings("9,2");
+		var cadena = new CalculadoraStrings("11,11");
+
+		Assertions.assertEquals(cadena.stringCalculator(cadena.getCadena()), 22);
+	}
+
+	@Test
+	@DisplayName("Dos números, delimitados por saltos de línea, devuelve la suma.")
+	void sumaPorSaltoLinea(){
+
+		var cadena = new CalculadoraStrings("2\n9");
 
 		Assertions.assertEquals(cadena.stringCalculator(cadena.getCadena()), 11);
+	}
+
+	@Test
+	@DisplayName("Los números negativos arrojan una excepción")
+	void numeroNegativo(){
+
+		var cadena = new CalculadoraStrings("-11");
+
+		IllegalArgumentException thrown = assertThrows(
+				IllegalArgumentException.class,
+				() -> cadena.stringCalculator(cadena.getCadena()),
+				"Esperaba el método stringCalculator, pero no se encontró este método dentro de la clase"
+		);
+
+		assertTrue(thrown.getMessage().contains("Numero negativo"));
+	}
+
+	@Test
+	@DisplayName("Los números superiores a 1000 se ignoran")
+	void numeroMayorAMil(){
+
+		var cadena = new CalculadoraStrings("1111");
+
+		Assertions.assertEquals(cadena.stringCalculator(cadena.getCadena()),0);
+	}
+
+	@Test
+	@DisplayName("Tres números, delimitados de cualquier manera, devuelven la suma")
+	void sumaTresNumeros(){
+
+		var cadena1 = new CalculadoraStrings("11 22 33");
+		var cadena2 = new CalculadoraStrings("11,22,33");
+		var cadena3 = new CalculadoraStrings("11#22#33");
+		var cadena4 = new CalculadoraStrings("11\n22\n33");
+		var cadena5 = new CalculadoraStrings("11/22/33");
+
+		Assertions.assertEquals(cadena1.stringCalculator(cadena1.getCadena()), 66);
+		Assertions.assertEquals(cadena2.stringCalculator(cadena2.getCadena()), 66);
+		Assertions.assertEquals(cadena3.stringCalculator(cadena3.getCadena()), 66);
+		Assertions.assertEquals(cadena4.stringCalculator(cadena4.getCadena()), 66);
+		Assertions.assertEquals(cadena5.stringCalculator(cadena5.getCadena()), 66);
 	}
 
 }
